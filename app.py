b2f83894f42406e94edf87552950e4449bfc5862
Chih Pin Chien <ryohei3364@gmail.com>
2025-03-11 21:07:08 +0800

    #2 upload app.py and data_process.py

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from fastapi import *
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
+from dotenv import load_dotenv
+import mysql.connector, os
+
+load_dotenv()
+MYSQL_PW=os.getenv("MYSQL_PW")
+
 app=FastAPI()
+
+con = mysql.connector.pooling.MySQLConnectionPool(
+  pool_name = "mypool",
+  pool_size = 10,
+  user = "root",
+  password = MYSQL_PW,
+  host = "localhost",
+  database = "taipei_trip",
+)
+
+cnx=con.get_connection()
+cursor = cnx.cursor(dictionary=True)
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
@@ -15,3 +33,98 @@ async def booking(request: Request):
 @app.get("/thankyou", include_in_schema=False)
 async def thankyou(request: Request):
 	return FileResponse("./static/thankyou.html", media_type="text/html")
+
+
+# 取得景點資料列表 /api/attractions?page=int&keyword=str
+@app.get("/api/attractions", include_in_schema=False)
+async def search(request: Request, page: int=0, keyword: str=None):
+  PAGE_SIZE = 12
+  offset = page * PAGE_SIZE
+  
+  if keyword:
+    query = """
+      SELECT id, name, category, description, address, transport, mrt, lat, lng, images
+      FROM attraction WHERE name LIKE %s OR mrt=%s LIMIT %s OFFSET %s
+    """
+    keyword_param=f"%{keyword}%"
+    params = (keyword_param, keyword, PAGE_SIZE, offset)
+    print(params)
+    
+    cursor.execute(query, params)
+    attractions = cursor.fetchall()
+    
+    if not attractions:
+      return {
+        "nextPage": None,
+        "data": [],
+      }
+    else:
+      return {
+        "nextPage": page + 1,
+        "data": attractions,
+      }
+  else:
+    return JSONResponse(
+      status_code=500,
+      content={
+        "error": True,
+        "message": "請輸入關鍵字查詢"
+      }
+    )
+
+# 根據景點編號取得景點資料 /api/attraction/{attractionId}
+@app.get("/api/attraction/{attractionId}", include_in_schema=False)
+async def attraction_id(request: Request, attractionId: int):
+  if attractionId:
+    query = """
+      SELECT id, name, category, description, address, transport, mrt, lat, lng, images
+      FROM attraction WHERE data_id=%s
+    """
+    cursor.execute(query, (attractionId,))
+    data = cursor.fetchall()
+    if data:
+      return {
+        "data": data
+      }
+    else:
+      return JSONResponse(
+        status_code=400,
+        content={
+          "error": True,
+          "message": "景點編號錯誤"
+        }
+      )
+  else:
+    return JSONResponse(
+      status_code=500,
+      content={
+        "error": True,
+        "message": "請輸入景點編號查詢"
+      }
+    )
+
+
+# 取得捷運站名稱列表 /api/mrts
+@app.get("/api/mrts", include_in_schema=False)
+async def station(request: Request):
+  query = """
+      SELECT mrt, COUNT(mrt) FROM attraction 
+      GROUP BY mrt ORDER BY COUNT(mrt) DESC
+    """ 
+  cursor.execute(query)
+  data = cursor.fetchall()
+  
+  if data:
+    mrt_list = [mrt["mrt"] for mrt in data if mrt and mrt.get("mrt") is not None]
+    return {
+      "data": mrt_list
+    }
+  else:
+    return JSONResponse(
+      status_code=500,
+      content={
+        "error": True,
+        "message": "查無資料"
+      }
+    )
+  
---
+++ data/data_process.py
@@ -0,0 +1,52 @@
+# arrange json data into MySQL database
+
+from dotenv import load_dotenv
+import mysql.connector, os, json, re
+
+load_dotenv()
+MYSQL_PW=os.getenv("MYSQL_PW")
+
+con = mysql.connector.pooling.MySQLConnectionPool(
+  pool_name = "mypool",
+  pool_size = 10,
+  user = "root",
+  password = MYSQL_PW,
+  host = "localhost",
+  database = "taipei_trip",
+)
+
+cnx=con.get_connection()
+cursor = cnx.cursor()
+
+with open('data/taipei-attractions.json', 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
+
+spots = data['result']['results']
+query = """
+  INSERT INTO attraction (data_id, name, category, description, address, transport, mrt, lat, lng, images)
+  VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
+for spot in spots:
+  data_id = spot['_id']
+  name = spot['name']
+  category = spot['CAT']
+  description = spot['description']
+  address = spot['address']
+  transport = spot['direction']
+  mrt = spot['MRT']
+  lat = spot['latitude']
+  lng = spot['longitude']
+  file = spot['file'].split("https://")
+  images = []
+  for img in file:
+    if img:
+      match = re.search(r"(^https?:(.{10,})*.(jpg|png))", "https://" + img, re.I)
+      if match:
+        images.append(match.group(0))
+
+  images_json = json.dumps(images)
+  val = (data_id, name, category, description, address, transport, mrt, lat, lng, images_json)
+  cursor.execute(query, val)  
+
+cnx.commit()
