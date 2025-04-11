343c6e5d4aae1561543506225ff663186efa358b
Chih Pin Chien <ryohei3364@gmail.com>
2025-04-11 10:42:54 +0800

    #2 fixed the date calendar error in mobile version

--- static/script/member.js
+++ static/script/member.js
@@ -4,11 +4,15 @@ const signInDialog = document.getElementById('signInDialog');
 const signUpDialog = document.getElementById('signUpDialog');
 const closeButtons = document.querySelectorAll('.close');
 const navBookingBtn = document.querySelector('.menu__item--container--booking');
+const dateInput = document.getElementById('date');
 const token = localStorage.getItem("token");
 
-function updateUI(token) {
-  signinButton.style.display = token ? "none" : "block";
-  signoutButton.style.display = token ? "block" : "none";
+const today = new Date().toISOString().split('T')[0];
+dateInput.value = today;
+
+function updateUI(isAuthenticated) {
+  signinButton.style.display = isAuthenticated ? "none" : "block";
+  signoutButton.style.display = isAuthenticated ? "block" : "none";
 }
 
 async function checkUserInfo() {
